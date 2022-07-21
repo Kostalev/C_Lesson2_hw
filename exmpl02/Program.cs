37f184762c6129cc76e4763e85f1ce3e9df914f0
kostalev <arctic@list.ru>
2022-07-21 19:59:44 +0500

    Changed

---
+++ exmpl02/Program.cs
@@ -0,0 +1,67 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// ЕСЛИ ИСКОМАЯ ЦИФРА НАЧИНАЕТСЯ ПРИ ОТСЧЕТЕ СПРАВА!!!!!!!!!!
+
+
+/*int inpt = new Random().Next();
+int x = inpt / 100;
+int result = x % 10;
+
+Console.WriteLine("Введенное число: {0}", inpt);
+
+if (x > 0)
+{
+    Console.WriteLine("Третья цифра в числе: {0}", result);
+}
+else
+{
+    Console.WriteLine("Третьей цифры в числе нет");
+}*/
+
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// ЕСЛИ ИСКОМАЯ ЦИФРА НАЧИНАЕТСЯ ПРИ ОТСЧЕТЕ СЛЕВА!!!!!!!!!!
+
+
+
+/*int inpt = new Random().Next();
+int n = 1;
+int res = 1;
+
+if (inpt >= 100)
+{
+    while (n < inpt)
+    {
+        n = n * 10;
+    }
+
+    n = n / 100;
+    res = inpt % n;
+
+    n = n / 10;
+    int result = res / n;
+    Console.WriteLine("третья цифра заданного числа {0}", result);
+}
+
+else
+    Console.WriteLine("третьей цифры нет");
+
+Console.WriteLine(inpt);*/
+
+
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// ЕСЛИ ИСКОМАЯ ЦИФРА НАЧИНАЕТСЯ ПРИ ОТСЧЕТЕ СЛЕВА!!!!!!!!!! (с помощью конвертации и субстроки)
+
+int inpt = new Random().Next();
+string str = Convert.ToString(inpt);
+int len = str.Length;
+
+if (len > 2)
+{
+    string res = str.Substring(2, 1);
+    int result = Convert.ToInt32(res);
+    Console.WriteLine("третья цифра заданного числа {0}:", result);
+}
+else
+    Console.WriteLine("третьей цифры нет");
+
+Console.WriteLine(inpt);
---
+++ exmpl03/Program.cs
@@ -0,0 +1,67 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+/*Console.WriteLine("Введите цифру дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+switch (day)
+{
+    case 1:
+        day = 1;
+        Console.WriteLine("Понедельник рабочий день");
+        break;
+
+    case 2:
+        day = 2;
+        Console.WriteLine("Вторник рабочий день");
+        break;
+
+    case 3:
+        day = 3;
+        Console.WriteLine("Среда рабочий день");
+        break;
+
+    case 4:
+        day = 4;
+        Console.WriteLine("Четверг рабочий день");
+        break;
+
+    case 5:
+        day = 5;
+        Console.WriteLine("Пятница рабочий день");
+        break;
+
+    case 6:
+        day = 6;
+        Console.WriteLine("Суббота выходной день");
+        break;
+
+    case 7:
+        day = 7;
+        Console.WriteLine("Воскресенье выходной день");
+        break;
+
+    default:
+
+        Console.WriteLine("В неделе всего семь дней");
+        break;
+
+}*/
+
+
+Console.WriteLine("Введите цифру дня недели: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+if (day >= 1 & day <= 5)
+{
+    Console.WriteLine("С понедельника по пятницу рабочий день");
+}
+if (day >= 6 & day <= 7)
+{
+    Console.WriteLine("Суббота и воскресенье выходной день");
+}
+if (day < 1 | day > 7)
+{
+    Console.WriteLine("В неделе всего семь дней");
+}
+
+
